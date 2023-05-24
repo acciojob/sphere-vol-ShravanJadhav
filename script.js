1ad71248d3c78bb707de5d3dd653f1c0ebaed58c
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-05-24 11:38:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
-    //Write your code here
-  
+
+	let r = document.getElementById("radius").value;
+	let v = (4/3) * Math.PI * Math.pow(r,3;
+
+	document.getElementById("value").value=v;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
